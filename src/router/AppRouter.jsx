06092bb80102f41ceaf3c817c4b9f18f6e3ddfbe
Cüneyt Ekrem ARI <cuneyt7088@gmail.com>
2023-03-30 17:37:35 +0300

    Pages created and their paths have been edited.

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { GlobalStyles } from '../components/globalStyles/GlobalStyles'
+import Navbar from "../components/nav/Navbar"
+import Home from "../pages/home/Home"
+import Login from "../pages/login/Login"
+import About from "../pages/about/About"
+import Detail from "../pages/detail/Detail"
+import PrivateRouter from "../router/PrivateRouter"
+
+const AppRouter = () => {
+  return (
+    <div>
+      <BrowserRouter>
+      <GlobalStyles/>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='login' element={<Login/>}/>
+          <Route path='about' element={<PrivateRouter/>}>
+            <Route path='' element={<About/>}/>
+          </Route>
+          <Route path='detail' element={<PrivateRouter/>}>
+            <Route path='' element={<Detail/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default AppRouter
